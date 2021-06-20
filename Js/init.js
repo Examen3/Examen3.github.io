4bbe5ee62c90375e7a4f3690aaad6c9e055f9240
Examen3 <86176422+Examen3@users.noreply.github.com>
2021-06-20 17:11:26 -0500

    Add files via upload

---
+++ Js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+  apiKey: "AIzaSyDLy5rMDz2XOIYhchPKPz6h6uoqjjZwsQw",
+    authDomain: "pruebagithub-e38b7.firebaseapp.com",
+    projectId: "pruebagithub-e38b7",
+    storageBucket: "pruebagithub-e38b7.appspot.com",
+    messagingSenderId: "913406786854",
+    appId: "1:913406786854:web:2bc49423883d3bbbaf55e7",
+    measurementId: "G-KEXXR25SDR"
+  });
